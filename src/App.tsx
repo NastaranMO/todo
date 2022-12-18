5b474df4648a3beb59a62535815e43ce2510f9db
Nastaran Moghadasi <nastaran.moghadasi@gmail.com>
2022-12-18 14:14:17 +0100

    add images folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <div className='App'>Hello</div>;
+};
+
+//interface, type extends
+//neucha font
+//position relative to put button inside of input
+//focus input box-shadow all;
+//usestate<string>('')
+//interface Props
+//always for each function component put the type React.fc<props>
+//put the type in front of usestate
+//e: rract.formevent
+//useref
+//react icons/ai or /md => search i doc
 
 export default App;
