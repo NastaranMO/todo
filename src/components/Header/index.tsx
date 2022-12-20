8b276cdf6a0122df579410eaf2181debd7997284
Nastaran Moghadasi <nastaran.moghadasi@gmail.com>
2022-12-20 15:07:55 +0100

    add title of header and darkmode css

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './index.scss';
+import sun from '../../images/icon-sun.svg';
+import moon from '../../images/icon-moon.svg';
 
 const Index = () => {
+  const theme = 'light';
   return (
-    <div className='light'>
+    <div className={`header ${theme}`}>
       <div className='headerImg'></div>
-      <h1>Todo</h1>
+      <div className='header__container'>
+        <h1 className='header__title'>Todo</h1>
+        <img src={theme === 'light' ? moon : sun} alt='moon' />
+      </div>
     </div>
   );
 };
