8e219713abb75f9f5a0ed06c432305b56d57ea24
Nastaran Moghadasi <nastaran.moghadasi@gmail.com>
2022-12-22 20:41:47 +0100

    add dark mode swicth func

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -5,12 +5,21 @@ import sun from '../../images/icon-sun.svg';
 import moon from '../../images/icon-moon.svg';
 
 const Index = () => {
-  const { theme } = useContext(ThemeContext);
+  const { theme, setTheme } = useContext(ThemeContext);
   const imgAlt = theme === 'light' ? 'moon' : 'sun';
   return (
     <div className='title'>
       <h1>Todo</h1>
-      <img src={theme === 'light' ? moon : sun} alt={imgAlt} />
+      <img
+        src={theme === 'light' ? moon : sun}
+        alt={imgAlt}
+        onClick={() => {
+          const currTheme = theme === 'light' ? 'dark' : 'light';
+          document.documentElement.classList.add(currTheme);
+          document.documentElement.classList.remove(theme);
+          setTheme(currTheme);
+        }}
+      />
     </div>
   );
 };
